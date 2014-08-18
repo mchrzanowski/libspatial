203cc7d7359b140aabe62da79fbeee9dd1bfe580
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2014-08-18 12:38:30 -0700

    remove taylor approx. delete binary in make clean

--- src/taylor_sar.h
+++
@@ -1,9 +0,0 @@
-#include "sar.h"
-
-class TaylorSAR : public SAR {
-public:
-  TaylorSAR(const arma::sp_mat &W, const arma::mat &X, const arma::colvec &y);
-
-protected:
-  double calc_log_det(double rho_hat);
-};
